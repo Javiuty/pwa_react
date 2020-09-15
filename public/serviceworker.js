f50aee7bad10ca4de05c9fbf7c6f38ef5b7f658b
Javiuty <javiiescuadra@gmail.com>
2020-09-15 11:52:19 +0200

    added images, manifest.json and serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -14,7 +14,28 @@ self.addEventListener("install", (event) => {
 });
 
 //LISTEN FOR REQUEST
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
 
 //ACTIVATE THE SW
-self.addEventListener("activate", (event) => {});
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
